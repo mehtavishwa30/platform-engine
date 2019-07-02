6f7ef04e0d67017dca4154b577c616b40b13e83e
Anukul Sangwan <anukulsangwan@icloud.com>
2019-07-02 09:55:47 +0200

    Updates after review

--- asyncy/Kubernetes.py
+++ asyncy/Kubernetes.py
@@ -566,7 +566,8 @@ class Kubernetes:
 
         liveness_probe = cls.get_liveness_probe(app, service_name)
 
-        limits = Database.get_service_limits(app.config, service_name)
+        tag = image.split(':')[-1]
+        limits = Database.get_service_limits(app.config, service_name, tag)
 
         payload = {
             'apiVersion': 'apps/v1',
